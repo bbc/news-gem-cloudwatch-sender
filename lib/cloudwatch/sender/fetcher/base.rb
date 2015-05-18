ee2305f62c25b3781f67ffebc5c3356b8d5c47db
Charlie Revett <charlierevett@gmail.com>
2015-05-18 17:05:36 +0100

    Tidy up base Fetcher class

--- lib/cloudwatch/sender/fetcher/base.rb
+++ lib/cloudwatch/sender/fetcher/base.rb
@@ -1,10 +1,12 @@
+require "cloudwatch/sender/base"
+
 module Cloudwatch
   module Sender
     module Fetcher
       class Base
         def initialize(components, namespace)
           @components = components
-          @namespace = namespace
+          @namespace  = namespace
         end
 
         def retrieve
@@ -17,29 +19,39 @@ module Cloudwatch
 
         private
 
-        attr_reader :components, :metric_prefix, :cloudwatch, :sender, :namespace
-
-        def metric_prefix
-          components["metric_prefix"]
-        end
-
-        def sender
-          Cloudwatch::Sender::Base.new(components["influx_host"], components["influx_port"])
-        end
+        attr_reader :components, :namespace
 
         def cloudwatch
           Aws::CloudWatch::Client.new
         end
 
+        def extract_class_name(component_meta)
+          component_meta["namespace"].split("/").last
+        end
+
+        def fetcher(component_meta)
+          namespace.start_with?("AWS/") ? Object.const_get(id component_meta)
+                                        : Cloudwatch::Sender::Fetcher::Custom
+        end
+
+        def id(component_meta)
+          "Cloudwatch::Sender::Fetcher::#{extract_class_name component_meta}"
+        end
+
+        def metric_prefix
+          components["metric_prefix"]
+        end
+
         def metric_type(component_meta, metric)
-          if namespace.start_with?("AWS/")
-            id = "Cloudwatch::Sender::Fetcher::#{component_meta['namespace'].split('/').last}"
-            build_message = Object.const_get(id).new(cloudwatch, sender, metric_prefix)
-            build_message.metrics(component_meta, metric)
-          else
-            build_message = Cloudwatch::Sender::Fetcher::Custom.new(cloudwatch, sender, metric_prefix)
-            build_message.metrics(component_meta, metric)
-          end
+          fetcher(component_meta).new(
+            cloudwatch, sender, metric_prefix
+          ).metrics(component_meta, metric)
+        end
+
+        def sender
+          Cloudwatch::Sender::Base.new(
+            components["influx_host"], components["influx_port"]
+          )
         end
       end
     end
