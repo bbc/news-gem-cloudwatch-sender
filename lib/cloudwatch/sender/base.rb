a2afa33539cdf021a973b145bdec7f7c00d6bcf4
Charlie Revett <charlierevett@gmail.com>
2015-05-18 17:04:47 +0100

    Rename base Sender class

--- lib/cloudwatch/sender/base.rb
+++ lib/cloudwatch/sender/base.rb
@@ -4,21 +4,22 @@ require "yaml"
 module Cloudwatch
   module Sender
     class Base
-      attr_accessor :influx_server, :influx_port
-
       def initialize(server, port)
         @influx_server = server
         @influx_port   = port
       end
 
       def send_tcp(contents)
-        sock = TCPSocket.open(influx_server, influx_port)
-        sock.print(contents)
+        TCPSocket.open(influx_server, influx_port).print contents
       rescue StandardError => e
-        logger.debug("Error : #{e}")
+        logger.debug "Error: #{e}"
       ensure
         sock.close
       end
+
+      protected
+
+      attr_reader :influx_server, :influx_port
     end
   end
 end
